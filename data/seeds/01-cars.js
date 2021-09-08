dfebba449708b5d2046a5f1d1d49848f38bc0849
RimshaSaleem <rimshatif8@gmail.com>
2021-09-07 20:48:41 -0700

    wi

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,30 @@
 // STRETCH
+const cars =[
+    {
+        vin: '11111111111',
+        make:'toyota',
+        model:'prius',
+        mileage:215000,
+        title:'clean',
+        transmissin:'manual',
+    },
+    {
+        vin: '11111111111',
+        make:'toyota',
+        model:'corollas',
+        mileage:115000,
+        title:'clean',
+        transmissin:'salvage',
+    },
+    {
+        vin: '11111111111',
+        make:'ford',
+        model:'focus',
+        mileage:115000,
+        
+    },
+]
+exports.seed =  async function(knex){
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
